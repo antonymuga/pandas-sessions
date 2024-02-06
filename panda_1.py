aa84d3616a0b8efbe6fe6570a1acfcd0a60d92b4
Antony Muga <anthony.ochieng.muga@gmail.com>
2024-02-06 15:05:13 +0300

    Added code to read different file types

---
+++ panda_1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+pkm_data_csv = pd.read_csv("pokemon_data.csv")
+
+# pkm_data_xlsx = pd.read_excel("pokemon_data.xlsx").head(20)
+
+# pkm_data_txt = pd.read_csv("pokemon_data.txt", delimiter='\t').tail(20)
